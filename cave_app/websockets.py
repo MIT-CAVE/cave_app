7a08299d92866a5d054cb7017ed8448a054c8281
Connor Makowski <connorman528@gmail.com>
2022-09-29 13:38:44 -0400

    Clean up socket code and add fx for ws sending to server

--- cave_app/websockets.py
+++ cave_app/websockets.py
@@ -1,4 +1,5 @@
 from django.urls import path
+import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -15,10 +16,15 @@ class AppMessageConsumer(AsyncWebsocketConsumer):
     # Broadcast app messages to clients
     async def broadcast(self, event):
         """
-        Broadcast events should always include a `payload`
+        Broadcast `event`s should always include a `payload`
         object that is JSON serialized
         """
         await self.send(event.get("payload", "{}"))
+
+    # Receive app messages from clients
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        #TODO: Use Data to do things
 
 
 websocket_urlpatterns = [
