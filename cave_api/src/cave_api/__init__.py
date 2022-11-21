d7b26cbfdfe5358a0b2f47b47f8a12ba4c070cdd
Connor Makowski <connorman528@gmail.com>
2022-11-21 16:26:55 -0500

    Update Static Model Example

--- cave_api/src/cave_api/__init__.py
+++ cave_api/src/cave_api/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.version_info[0] == 3:
-    from .serialization_model.api import execute_command
+    from .static_model.api import execute_command
 elif sys.version_info[0] < 3:
     raise Exception("This package only supports python3")
