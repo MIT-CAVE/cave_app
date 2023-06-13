1b2a7476ed254d72a022ef1e97cc88124e472d79
Max Katz-Christy <maxkatzchristy@gmail.com>
2023-06-13 14:42:27 -0400

    allow one csrf origin

--- cave_app/settings/development.py
+++ cave_app/settings/development.py
@@ -32,6 +32,10 @@ SECRET_KEY = config("SECRET_KEY")
 ## Allow All Hosts For Development
 ### NOTE: This Should be explicit in a production setting
 ALLOWED_HOSTS = ["*"]
+##
+allowed_host = os.environ.get("CSRF_TRUSTED_ORIGIN")
+if allowed_host:
+    CSRF_TRUSTED_ORIGINS = [f"https://{allowed_host}"]
 ## Middleware
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
