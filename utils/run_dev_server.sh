1c5f3f7eb8601f2ac38a5a1e0d9279eb9be13239
Connor Makowski <connorman528@gmail.com>
2023-06-21 11:01:53 -0400

    Docker Bug Fix: Get functional on apple silicon

--- utils/run_dev_server.sh
+++ utils/run_dev_server.sh
@@ -8,7 +8,7 @@ APP_DIR=$(dirname "$SCRIPT_DIR")
 # Poll for postgres to be up
 while ! (source /app/.env && python3 -c "import psycopg2; psycopg2.connect(\"dbname='$DATABASE_NAME' user='$DATABASE_USER' host='$DATABASE_HOST' password='$DATABASE_PASSWORD'\")" 2>/dev/null) do
   printf "Waiting for postgres...\n"
-  sleep 1
+  sleep 3
 done
 
 python "$APP_DIR/manage.py" makemigrations cave_core --deployment_type development
