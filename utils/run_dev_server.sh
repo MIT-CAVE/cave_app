31c13c87c5e2a3c71348e13be99dfc53b59b6294
Connor Makowski <connorman528@gmail.com>
2023-06-21 13:08:36 -0400

    Timeout the postgres check

--- utils/run_dev_server.sh
+++ utils/run_dev_server.sh
@@ -6,8 +6,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR=$(dirname "$SCRIPT_DIR")
 
 # Poll for postgres to be up
-while ! (source /app/.env && python3 -c "import psycopg2; psycopg2.connect(\"dbname='$DATABASE_NAME' user='$DATABASE_USER' host='$DATABASE_HOST' password='$DATABASE_PASSWORD'\")" 2>/dev/null) do
-  printf "Waiting for postgres...\n"
+for i in {1..6}; do
+  if [ ! $i -eq 6 ]; then
+    if (source /app/.env && python3 -c "import psycopg2; psycopg2.connect(\"dbname='$DATABASE_NAME' user='$DATABASE_USER' host='$DATABASE_HOST' password='$DATABASE_PASSWORD'\")" 2>/dev/null); then
+      break
+    fi
+  else
+    printf "Postgres never came up...\nMaking one last attempt and logging any errors that occur:\n"
+    if (source /app/.env && python3 -c "import psycopg2; psycopg2.connect(\"dbname='$DATABASE_NAME' user='$DATABASE_USER' host='$DATABASE_HOST' password='$DATABASE_PASSWORD'\")"); then
+      break
+    else
+      printf "Postgres never came up, exiting...\n"
+      exit 1
+    fi
+  fi
+  printf "Waiting for postgres ($i)...\n"
   sleep 3
 done
 
