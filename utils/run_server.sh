7b51e2492d8bfedee43a3bf719a16a732c6d1808
Connor Makowski <connorman528@gmail.com>
2023-06-27 15:18:55 -0400

    To cave run, ensure the db has been setup first.

--- utils/run_server.sh
+++ utils/run_server.sh
@@ -24,4 +24,24 @@ for i in {1..6}; do
   sleep 3
 done
 
+# Offer to reset the db if it has not yet been setup
+if [ "$(python ./manage.py showmigrations --deployment_type development | grep "\[X\]" | wc -l)" -eq "0" ]; then
+  printf "Your database has not been setup or is not properly configured." 2>&1 | pipe_log "WARN"
+  # Ask the user if they want to reset the db
+  printf "Would you like to reset the database using './utils/reset_db.sh'? (y/n): " 2>&1 | pipe_log "WARN"
+  read -r reset_db
+  if [ "$reset_db" = "y" ]; then
+    printf "Resetting the database...\n" 2>&1 | pipe_log "WARN"
+    source ./utils/reset_db.sh
+    # Check if the db was reset successfully
+    if [ "$(python ./manage.py showmigrations --deployment_type development | grep "\[X\]" | wc -l)" -eq "0" ]; then
+      printf "Unable to reset the database.\n" 2>&1 | pipe_log "ERROR"
+      exit 1
+    fi
+  else
+    printf "Unable to start the app. Ensure your database is properly configured.\n" 2>&1 | pipe_log "ERROR"
+    exit 1
+  fi
+fi
+
 python "$APP_DIR/manage.py" runserver 0.0.0.0:8000 2>&1 | pipe_log "INFO"
