9a26ffb6535614a1dde0888daf4da32d2214e94e
Connor Makowski <connorman528@gmail.com>
2023-06-28 14:45:24 -0400

    Add in helper scripts

--- utils/run_server.sh
+++ utils/run_server.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR=$(dirname "$SCRIPT_DIR")
 
 source /cave_cli/utils.sh
-source ./utils/helpers/cli_backup.sh
+source ./utils/helpers/utils_backup.sh
 source ./utils/helpers/ensure_postgres_running.sh
 source ./utils/helpers/ensure_db_setup.sh
 
---
+++ utils/run_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+APP_DIR=$(dirname "$SCRIPT_DIR")
+
+source /cave_cli/utils.sh
+source ./utils/helpers/utils_backup.sh
+  
+
+ALL_FLAG=$(has_flag -all "$@")
+
+if [[ ! -f "./cave_api/tests/$1" && "${ALL_FLAG}" != "true" ]]; then
+    printf "Test $1 not found. Ensure you entered a valid test name.\n" | pipe_log "ERROR"
+    printf "Tests available in 'cave_api/tests/' include \n $(ls cave_api/tests/)\n" | pipe_log "ERROR"
+    exit 1
+fi
+
+printf "\n" | pipe_log "INFO"
+# Run given test in docker
+if [ "${ALL_FLAG}" != "true" ]; then
+    python "./cave_api/tests/$1" 2>&1 | pipe_log "INFO"
+else
+    for f in ./cave_api/tests/*.py; do
+        python "./$f" 2>&1 | pipe_log "INFO"
+    done
+fi
