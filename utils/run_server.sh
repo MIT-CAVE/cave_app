b8b87c4aa02809efafd8ff5dbf5b1991ee28c864
Connor Makowski <connorman528@gmail.com>
2025-02-27 14:54:19 -0500

    Add check to reset and run commands to better handle python bugs without raising an error that the db needs to be reset when it does not.

--- utils/run_server.sh
+++ utils/run_server.sh
@@ -5,6 +5,12 @@ APP_DIR=$(dirname "$SCRIPT_DIR")
 
 source ./utils/helpers/shell_functions.sh
 source ./utils/helpers/ensure_postgres_running.sh
+# Check if the app is functional before proceeding
+if [ "$(python ./manage.py check --deployment_type development | grep "System check identified no issues" | wc -l)" -eq "0" ]; then
+  printf "Unable to start the app due to an error in the code. See the stacktrace above." 2>&1 | pipe_log "ERROR"
+  rm -r "./tmp"
+  exit 1
+fi
 source ./utils/helpers/ensure_db_setup.sh
 
 python "$APP_DIR/manage.py" runserver 0.0.0.0:8000 2>&1 | pipe_log "INFO"
