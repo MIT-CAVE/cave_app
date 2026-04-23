7227c5b6f288024dd1f5c6f42fb34388f1a410a3
Connor Makowski <connorman528@gmail.com>
2026-04-23 15:43:19 -0400

    Make output more pretty when running tests

--- utils/run_test.sh
+++ utils/run_test.sh
@@ -1,20 +1,21 @@
-#!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR=$(dirname "$SCRIPT_DIR")
 
 source ./utils/helpers/shell_functions.sh
-  
-if [[ ! -f "./cave_api/tests/$3" && "${ALL_FLAG}" != "true" ]]; then
-    printf "Test $3 not found. Ensure you entered a valid test name.\n" | pipe_log "ERROR"
-    printf "Tests available in 'cave_api/tests/' include \n $(ls cave_api/tests/)\n" | pipe_log "ERROR"
+
+if [[ ! -f "./cave_api/tests/$1" && "${ALL_FLAG}" != "true" ]]; then
+    printf "Test $1 not found. Ensure you entered a valid test name.\n" | pipe_log "ERROR"
+    printf "Tests available in 'cave_api/tests/' include:\n" | pipe_log "ERROR"
+    for f in ./cave_api/tests/*.py; do
+        printf "  $(basename $f)\n" | pipe_log "ERROR"
+    done
     exit 1
 fi
 
 printf "\n" | pipe_log "INFO"
-# Run given test in docker
 if [ "${ALL_FLAG}" != "true" ]; then
-    python "./cave_api/tests/$3" 2>&1 | pipe_log "INFO"
+    python "./cave_api/tests/$1" 2>&1 | pipe_log "INFO"
 else
     for f in ./cave_api/tests/*.py; do
         python "./$f" 2>&1 | pipe_log "INFO"
